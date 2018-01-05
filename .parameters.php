5c96667cb30143198dd825c07605e4dc6aad2d45
Denis M <gdecider@gmail.com>
2018-01-05 16:10:46 +0300

    Update .parameters.php

--- .parameters.php
+++ .parameters.php
@@ -1,5 +1,11 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+/**
+ * @var string $componentPath
+ * @var string $componentName
+ * @var array $arCurrentValues
+ * */
+
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 
